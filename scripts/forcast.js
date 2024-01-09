9a7513eaf7d8df3ba784abd352208a116bfcb620
Kevin Asogwa <kevinasogwa98@gmail.com>
2024-01-09 09:40:36 +0100

    (updated city and weather information)

--- scripts/forcast.js
+++ scripts/forcast.js
@@ -1,1 +1,28 @@
-const key = "iLCBBxpoODcz4GA8Pco7pbwApFPlD7RJ";
+const key = "YfifKAmON1XXJO4GTAbjvBdxG0PUBAsn";
+
+//get weather information
+const getWeather = async (id) => {
+  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+//get city information
+const getCity = async (city) => {
+  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+getCity("manchester")
+  .then((data) => {
+    return getWeather(data.Key);
+  })
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
