3c75186edd8be08a0c4d89547c646e42098f8207
Kevin Asogwa <kevinasogwa98@gmail.com>
2024-01-09 18:59:04 +0100

    (updated the UI of the weather app)

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,15 @@
 const cityForm = document.querySelector("form");
 const card = document.querySelector(".card");
 const details = document.querySelector(".details");
+const time = document.querySelector("img.time");
+const icon = document.querySelector(".icon img");
 
 const updateUI = (data) => {
-  const cityDets = data.cityDets;
-  const weather = data.weather;
+  // const cityDets = data.cityDets;
+  // const weather = data.weather;
+
+  //destructuring properties
+  const { cityDets, weather } = data;
 
   //update details template
   details.innerHTML = `
@@ -15,6 +20,23 @@ const updateUI = (data) => {
             <span>&deg;C</span>
             </div>
             `;
+
+  //update the night/day & icon images
+
+  //using tenary operator
+  let timeSrc = weather.IsDayTime ? "img/day.svg" : "img/night.svg";
+  time.setAttribute("src", timeSrc);
+
+  //or using if statement
+  // let timeSrc = null;
+  // if (weather.IsDayTime) {
+  //   timeSrc = "img/day.svg";
+  // } else timeSrc = "img/night.svg";
+  // time.setAttribute("src", timeSrc);
+
+  const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+  icon.setAttribute("src", iconSrc);
+
   //remove the d-none class if present
   if (card.classList.contains("d-none")) {
     card.classList.remove("d-none");
